d55c34bca302bfce5bb0751e9a1654604dd1966e
Miriam Cortes <mcortes.umiami@gmail.com>
2016-11-29 09:28:24 -0800

    whoops forgot I needed that quote_view file

--- src/app.js
+++ src/app.js
@@ -42,39 +42,3 @@ $(document).ready(function() {
     // Call simulate() on each quote in the ApplicationView
   }, 1000);
 });
-
-var QuoteView = Backbone.View.extend({
-  initialize: function(options) {
-    this.stock = options.stock;
-    this.template = options.template;
-  }, //close initialize
-
-  events: {
-    'click .btn-buy': 'buyStock',
-    'click .btn-sell': 'sellStock',
-    // 'simulate': 'setInterval'
-  }, //close events
-
-  buyStock: function(event) {
-    event.preventDefault();
-    this.stock.price += 1
-    // this.simulate();
-    this.render();
-  }, //close buyStock
-
-  sellStock: function(event) {
-    event.preventDefault();
-    this.stock.price -= 1
-    this.render();
-  },//close sellStock
-
-  render: function() {
-    var html = this.template({stock: this.stock})
-    this.$el.html(html);
-
-    // Enable chained calls
-    return this;
-  }//close render
-});
-
-export default QuoteView;
--- src/app/views/application_view.js
+++ src/app/views/application_view.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import Backbone from 'backbone';
 import _ from 'underscore';
 
-import QuoteView from 'app.js';
+import QuoteView from 'app/views/quote_view';
 
 const ApplicationView = Backbone.View.extend({
   initialize: function(options) {
